e1a11b65c78b6365c93c4456d46a602ac3fd9b9b
Anna Pliskovskaya <anna@bkstg.com>
2018-05-20 16:48:34 +0300

    add footer to list

--- app/src/main/java/com/itrex/tasks/twittersearch/TweetsAdapter.java
+++ app/src/main/java/com/itrex/tasks/twittersearch/TweetsAdapter.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.RequestOptions;
@@ -16,12 +17,16 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * @author Anna Pliskovskaya (anna.pliskovskaya@gmail.com)
  * @since 5/20/18
  */
-public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder> {
+public class TweetsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int TYPE_ITEM = 1;
+    private static final int TYPE_FOOTER = 2;
+
     private List<Tweet> mResultsList;
 
     public TweetsAdapter(List<Tweet> list) {
@@ -30,28 +35,54 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false);
-        return new ViewHolder(convertView);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == TYPE_ITEM) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_layout, parent, false);
+            return new ItemViewHolder(view);
+        } else if (viewType == TYPE_FOOTER) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.footer_layout, parent, false);
+            return new FooterViewHolder(view);
+        }
+
+        throw new RuntimeException("there is no type that matches the type " + viewType + " + make sure your using types correctly");
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if (holder instanceof FooterViewHolder) {
+            return;
+        }
+
         final Tweet tweet = mResultsList.get(position);
-        holder.bind(tweet);
+        ((ItemViewHolder)holder).bind(tweet);
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (isPositionFooter(position)) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_ITEM;
+    }
+
+    private boolean isPositionFooter(int position) {
+        return position != 0 && position == getItemCount() - 1;
     }
 
     @Override
     public int getItemCount() {
-        return mResultsList.size();
+        if (mResultsList == null || mResultsList.size() == 0) {
+            return 0;
+        }
+        return mResultsList.size() + 1;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    public class ItemViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.user_avatar) ImageView mAvatar;
         @BindView(R.id.user_name) TextView mUserName;
         @BindView(R.id.tweet_text) TextView mText;
 
-        public ViewHolder(View itemView) {
+        public ItemViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
         }
@@ -65,6 +96,19 @@ public class TweetsAdapter extends RecyclerView.Adapter<TweetsAdapter.ViewHolder
 
             mUserName.setText(tweet.getUserName());
             mText.setText(tweet.getText());
+        }
+    }
+
+    public class FooterViewHolder extends RecyclerView.ViewHolder {
+        public FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, itemView);
+        }
+
+        @OnClick(R.id.footer_text)
+        public void loadMore() {
+            //TODO temp
+            Toast.makeText(itemView.getContext(), "Load more", Toast.LENGTH_SHORT).show();
         }
     }
 }
